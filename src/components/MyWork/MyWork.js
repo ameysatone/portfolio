bc536295aeb4d2748917d41a6fbed8066b651ccb
Ganesh Derkar <ganesh.derkar.dev@gmail.com>
2025-04-24 23:40:57 +0530

    Update MyWork.js
    added image

--- src/components/MyWork/MyWork.js
+++ src/components/MyWork/MyWork.js
@@ -7,7 +7,7 @@ const allProjects = [
   {
     title: 'Food delivery App',
     description: 'Food Delivery Application using react js Technologies Used React.js HTML CSS JavaScript API integration Key Features React Router DOM: Used for client-side routing. useState and useEffect: Leveraged React hooks for managing state and side effects. Custom Hooks: Created custom hooks to abstract and reuse stateful logic across components.',
-    image: 'https://via.placeholder.com/300',
+    image: '/assects/fooddeli.jpg',
     githubLink: 'https://github.com/GaneshDerkar18/food-delivery-app',
     demoLink: '#',
   },
